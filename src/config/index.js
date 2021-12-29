5bd6140bfee591175a7e75dbd6d5a8594b91bd3a
kingsoo123 <sabistore12@gmail.com>
2021-12-29 09:29:43 +0100

    loan endpoint consumed

--- src/config/index.js
+++ src/config/index.js
@@ -7,7 +7,7 @@ const config = () => {
     cache = Object.freeze({
       env,
       secrets: {
-        apiHost: "/api/adminservice-env.eba-ubpbf6se.us-east-2.elasticbeanstalk.com/index.php/api",
+        apiHost: "/api/index.php/api",
       },
     });
   }
